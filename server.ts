e49b769966a50e981b5607f16491a9bfb531b68f
Caccc <bakaneko8.22@gmail.com>
2026-05-07 21:08:02 +0700

    Fix error tipe data port

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 // Setup View Engine (EJS)
 app.set('views', path.join(__dirname, 'views'));
